9fa8888caebe014083c7c551a99a73392eab5e4c
hge2020 <hge2020@korea.ac.kr>
2023-09-13 20:41:07 +0900

    [MOD]공백인 경우에도 처리할 수 있게 코드 수정

--- network/C_process/C1_request_server/C1_server.py
+++ network/C_process/C1_request_server/C1_server.py
@@ -5,12 +5,16 @@ import threading
 app = FastAPI()
 
 class Item(BaseModel):
-    number: int
+    isnull: bool
+    data: int
 
 # 파일에 데이터를 추가하는 함수
 def append_to_file(item):
     with open("../C_public/example.txt", "a") as file: # TODO: you need to change when setting server sample script
-        file.write(str(item.number) + "\n")
+        if item.isnull:
+            file.write("\n")
+        else:
+            file.write(str(item.data) + "\n")
         file.close()
 
 # 파일 접근을 동기화하기 위한 Lock 객체 생성
@@ -19,10 +23,15 @@ file_lock = threading.Lock()
 @app.post("/")
 async def C1_server(item: Item): # TODO: you need to change when setting server sample script
     # 파일 접근을 Lock으로 동기화
-    item.number = item.number + 10 # TODO: you need to change when setting server sample script
-    with file_lock:
-        append_to_file(item)
-    return item
+    if item.isnull:
+        with file_lock:
+            append_to_file(item)
+        return "no data"
+    else:
+        item.data = item.data + 10 # TODO: you need to change when setting server sample script
+        with file_lock:
+            append_to_file(item)
+        return item
 
 def C1_run(): # TODO: you need to change when setting server sample script
     import uvicorn
