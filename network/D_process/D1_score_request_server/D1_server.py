f9d5b1ee2dd340523cc8ed71dbb28f2f45f1e28d
koo <gunmo1597@gmail.com>
2023-12-14 21:06:40 +0900

    [COM] D1_server

--- network/D_process/D1_score_request_server/D1_server.py
+++ network/D_process/D1_score_request_server/D1_server.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from datetime import time
-
 import threading
 import sys, os
 
@@ -20,7 +18,6 @@ app = FastAPI()
 class Item(BaseModel):
     request_id: int
 
-
 # 파일 접근을 동기화하기 위한 Lock 객체 생성
 file_lock = threading.Lock()
 
@@ -28,22 +25,12 @@ file_lock = threading.Lock()
 @app.post("/D1")
 async def D1_server(item: Item): # TODO: you need to change when setting server sample script
 
-    response = session.query(models.score_request_queue_tbl).filter(models.score_request_queue_tbl.request_id == item.request_id)
-    device_id = response[0].device_id
-    task_subgroup_code = response[0].task_subgroup_code
-
-    response1 = session.query(models.subgroup_detail_tbl).filter(models.subgroup_detail_tbl.task_subgroup_code == task_subgroup_code)
-    location_x = response1[0].location_x
-    location_y = response1[0].location_y
-    motion_code = response1[0].motion_code
-    sequence = response1[0].sequence
-    requested_start_time = response1[0].requested_start_time
-    insrt_ts = response1[0].insrt_ts
-    updt_ts = response1[0].updt_ts
+    response = session.query(models.score_request_queue_tbl).filter(models.score_request_queue_tbl.request_id == item.request_id)[0].task_subgroup_code
+    detail_tbl = session.query(models.subgroup_detail_tbl).filter(models.subgroup_detail_tbl.task_subgroup_code == response).all()
     
-    expected_score = location_x*10 + location_y*10 + motion_code + sequence
-    expected_time = time(15, 30, 0)
-
+    # AI로직 추가
+    # AI로직 추가
+    # AI로직 추가
 
     data = models.score_tbl(
     expected_score = expected_score, expected_time = expected_time, request_id = item.request_id
