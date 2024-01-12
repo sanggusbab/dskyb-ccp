b59cca0b46aa9f7218366942c48ceab3fe32148e
Sangmin Lee <sanggusbab@gmail.com>
2024-01-12 23:05:51 +0900

    develop branch modified

--- network/F_process/F2_report_client/F2_client.py
+++ network/F_process/F2_report_client/F2_client.py
@@ -1,13 +1,11 @@
 import httpx
 import json
 import sys, os
-
-#다른 폴더로 분리한 파일 가져오기위함.
+import time
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from F_public import database
 from F_public import models
 
-#기본세팅
 engine = database.engineconn()
 session = engine.sessionmaker()
 conn = engine.connection()
@@ -17,35 +15,47 @@ my_device_id = 1
 def drone_do(task):
     return task
 
-# 비동기 클라이언트 생성
-async def F2_client(): # TODO: you need to change when setting server sample script
-    with open("../F_public/data.json", "r") as json_file:
-        json_data = json.load(json_file)
-    data_list = json_data["data"]
-    if not len(data_list) == 0:
-        task = data_list.pop(0)
-        print(task)
-        assignment_tbl = session.query(models.assignment_tbl).filter((models.assignment_tbl.device_id == my_device_id)&(models.assignment_tbl.task_subgroup_code == task)).first()
-        if assignment_tbl.take_yn == 'n':
-            task = drone_do(task)
-            print("수행완료")
-            assignment_tbl.take_yn = 'y'
-            session.commit()
-            async with httpx.AsyncClient() as client:
-                response = await client.post("http://localhost:8005/", json={"assignment_id":assignment_tbl.index})
-            print("POST 요청 응답:", response.json())
-        else:print("이미 수행된 과제입니다.")
-        json_data["data"] = data_list
-        with open("../F_public/data.json", "w") as file:
-            json.dump(json_data, file, default=str)
-        
+def get_first_entry_from_json():
+    json_file_path = '../F_public/data.json'
+    try:
+        with open(json_file_path, 'r') as json_file:
+            existing_data = json.load(json_file)
+            if existing_data:
+                first_entry = existing_data[0]
+                # Remove the first entry from the JSON file
+                remaining_entries = existing_data[1:]
+                with open(json_file_path, 'w') as updated_json_file:
+                    json.dump(remaining_entries, updated_json_file, default=str)
+                return first_entry
+            else:
+                return None  # Return None if the file is empty
+    except (FileNotFoundError, json.JSONDecodeError, IndexError):
+        return None
+
+async def F2_client():
+    first_entry = get_first_entry_from_json()
+    if not first_entry:
+        print("No entries in data.json. Pause program 5 secs")
+        time.sleep(5)
+        return None
+    task = first_entry
+    print(task)
+    assignment_tbl = session.query(models.assignment_tbl).filter((models.assignment_tbl.device_id == my_device_id)&(models.assignment_tbl.task_subgroup_code == task)).first()
+    if assignment_tbl.take_yn == 'n':
+        task = drone_do(task)
+        print("?��?��?���?")
+        assignment_tbl.take_yn = 'y'
+        session.commit()
+        async with httpx.AsyncClient() as client:
+            response = await client.post("http://localhost:8005/", json={"assignment_id":assignment_tbl.index})
+        print("Response:", response.json())
+    else:print("?���? ?��?��?�� 과제?��?��?��.")
     return
 
-def F2_run(): # TODO: you need to change when setting server sample script
+def F2_run():
     import asyncio
     while True:
-        asyncio.run(F2_client()) # TODO: you need to change when setting server sample script
-
+        asyncio.run(F2_client())
 
 if __name__ == "__main__":
-    F2_run() # TODO: you need to change when setting server sample script
+    F2_run()
